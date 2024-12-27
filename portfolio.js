74b15ef1ce72f8e297746ef63cf4a74f8f5ceacb
Sky Harman <sky88822@users.noreply.github.com>
2024-12-27 14:42:34 +0000

    slideshow complete phew

--- portfolio.js
+++ portfolio.js
@@ -37,3 +37,13 @@ document.addEventListener("DOMContentLoaded", function() {
   changeWelcomeText(); // Initial call to display the first text
   setInterval(changeWelcomeText, 3000); // Change the text every 3 seconds
 });
+
+const root = document.documentElement;
+const marqueeElementsDisplayed = getComputedStyle(root).getPropertyValue("--marquee-elements-displayed");
+const marqueeContent = document.querySelector("ul.marquee-content");
+
+root.style.setProperty("--marquee-elements", marqueeContent.children.length);
+
+for(let i=0; i<marqueeElementsDisplayed; i++) {
+  marqueeContent.appendChild(marqueeContent.children[i].cloneNode(true));
+}
