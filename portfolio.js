e86849a5fad3b2c7fe7fa41dc1dfa0c6ed007c15
Sky Harman <sky88822@users.noreply.github.com>
2024-12-28 17:22:24 +0000

    day 4

--- portfolio.js
+++ portfolio.js
@@ -38,15 +38,15 @@ document.addEventListener("DOMContentLoaded", function() {
   setInterval(changeWelcomeText, 3000); // Change the text every 3 seconds
 });
 
-const root = document.documentElement;
-const marqueeElementsDisplayed = getComputedStyle(root).getPropertyValue("--marquee-elements-displayed");
-const marqueeContent = document.querySelector("ul.marquee-content");
+// const root = document.documentElement;
+// const marqueeElementsDisplayed = getComputedStyle(root).getPropertyValue("--marquee-elements-displayed");
+// const marqueeContent = document.querySelector("ul.marquee-content");
 
-root.style.setProperty("--marquee-elements", marqueeContent.children.length);
+// root.style.setProperty("--marquee-elements", marqueeContent.children.length);
 
-for(let i=0; i<marqueeElementsDisplayed; i++) {
-  marqueeContent.appendChild(marqueeContent.children[i].cloneNode(true));
-}
+// for(let i=0; i<marqueeElementsDisplayed; i++) {
+//   marqueeContent.appendChild(marqueeContent.children[i].cloneNode(true));
+// }
 
 
 
