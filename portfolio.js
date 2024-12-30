a45b0ebd435f5168d27c4e2a08d80c4bfebaf138
Sky Harman <sky88822@users.noreply.github.com>
2024-12-30 12:48:30 +0000

    9

--- portfolio.js
+++ portfolio.js
@@ -1,41 +1,39 @@
 document.addEventListener("DOMContentLoaded", function() {
-  // Array of "Welcome" translations
   const welcomeTranslations = ["Welcome", "Bienvenido", "Bienvenue", "Willkommen", "Bienvenue", "Benvenuto"];
   let currentTranslationIndex = 0;
 
   const welcomeTextElement = document.getElementById("welcome-text");
 
-  // Function to apply typewriting effect
   function typewrite(element, text, speed = 100) {
-      element.textContent = ''; // Clear the text initially
+      element.textContent = '';
       let i = 0;
 
       function typing() {
           if (i < text.length) {
-              element.textContent += text.charAt(i); // Add the next character
+              element.textContent += text.charAt(i);
               i++;
-              setTimeout(typing, speed); // Call typing again after a short delay
+              setTimeout(typing, speed);
           }
       }
 
-      typing(); // Start typing the text
+      typing();
   }
 
-  // Function to change the "Welcome" text every 3 seconds
+
   function changeWelcomeText() {
-      // Get the current translation
+
       const currentTranslation = welcomeTranslations[currentTranslationIndex];
 
-      // Apply typewriting effect to the current "Welcome" translation
+
       typewrite(welcomeTextElement, currentTranslation, 150);
 
-      // Update the index for the next translation
+
       currentTranslationIndex = (currentTranslationIndex + 1) % welcomeTranslations.length;
   }
 
-  // Start the welcome text change every 3 seconds
-  changeWelcomeText(); // Initial call to display the first text
-  setInterval(changeWelcomeText, 3000); // Change the text every 3 seconds
+
+  changeWelcomeText();
+  setInterval(changeWelcomeText, 3000);
 });
 
 // const root = document.documentElement;
@@ -49,10 +47,6 @@ document.addEventListener("DOMContentLoaded", function() {
 // }
 
 
-
-
-
-
 $('#manualCarousel').carousel({
-  interval: 3000 // Slide will change every 3 seconds, adjust if you want to change the interval
+  interval: 3000 
 });
